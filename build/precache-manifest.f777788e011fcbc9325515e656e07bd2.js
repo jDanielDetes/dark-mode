0e446dc5b6e1adbe3f130f43c48b1806c339d4fd
jDanielDetes <jdanieldetes@gmail.com>
2020-01-15 19:18:17 -0500

    update

---
+++ build/precache-manifest.f777788e011fcbc9325515e656e07bd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6cbb955aade91f39a1b486fbc61fe4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9fbff2884a974f9cd84",
+    "url": "/static/css/main.cfbed01c.chunk.css"
+  },
+  {
+    "revision": "fa40d41414ee0b9aebaa",
+    "url": "/static/js/2.88feffe3.chunk.js"
+  },
+  {
+    "revision": "b9fbff2884a974f9cd84",
+    "url": "/static/js/main.511e6330.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
